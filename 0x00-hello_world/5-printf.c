7bdfe5d16759279fa954a9ce4f2124ff3b1b94a9
Pandonyx <pandonyx@gmail.com>
2023-02-16 12:05:37 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program.
+* This program prints a string with proper grammar using the printf function.
+* The string is a piece of art, and is followed by a newline character.
+* The main function returns 0 to indicate successful execution.
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
