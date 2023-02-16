1d3c5972421312e18205df8804126fb7e65a1ff7
Pandonyx <pandonyx@gmail.com>
2023-02-16 12:57:16 -0800

    descri

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program.
- * This program outputs a string using the puts function.
- * The main function returns 0 to indicate successful execution.
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
