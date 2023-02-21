00bb698eefb64b27eef0fdd42483d551c9142641
Pandonyx <pandonyx@gmail.com>
2023-02-21 13:13:37 -0800

    to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,19 +4,27 @@
  * print_to_98 - Prints all natural numbers from n to 98
  * @n: The starting natural number
  */
-void print_to_98(int n) {
+void print_to_98(int n)
+{
 int i;
-if (n <= 98) {
-for (i = n; i <= 98; i++) {
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
 printf("%d", i);
-if (i != 98) {
+if (i != 98)
+{
 printf(", ");
 }
 }
-} else {
-for (i = n; i >= 98; i--) {
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
 printf("%d", i);
-if (i != 98) {
+if (i != 98)
+{
 printf(", ");
 }
 }
