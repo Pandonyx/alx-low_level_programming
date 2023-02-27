f07568f42205522f10ab6f48310dc35a0b45eb6f
Pandonyx <pandonyx@gmail.com>
2023-02-27 07:03:32 -0800

    errors?

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * puts2 - Prints every other character of a string to stdout, followed by a new line.
+ * puts2 - Prints every other character of a string to stdout,
+ * followed by a new line.
  * @str: The string to be printed.
- *
  * Return: void.
  */
 void puts2(char *str)
