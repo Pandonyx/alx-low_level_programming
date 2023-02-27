b07b70c8a40ec468fdcf5f6133f667360012df21
Pandonyx <pandonyx@gmail.com>
2023-02-27 06:26:16 -0800

    pstring

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - Prints every other character of a string to stdout, followed by a new line.
+ * @str: The string to be printed.
+ *
+ * Return: void.
+ */
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i % 2 == 0)
+putchar(str[i]);
+}
+putchar('\n');
+}
