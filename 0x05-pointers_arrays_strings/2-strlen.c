79984688fab908c97631db9ec459d6567c039927
Pandonyx <pandonyx@gmail.com>
2023-02-27 06:09:01 -0800

    we hate betty style

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,11 +6,13 @@
  *
  * Return: The length of the string.
  */
-int _strlen(char *s) {
+int _strlen(char *s)
+{
 int len = 0;
-while (*s != '\0') {
+while (*s != '\0')
+{
 len++;
 s++;
 }
-return len;
+return (len);
 }
