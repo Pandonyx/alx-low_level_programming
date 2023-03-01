e9b0adf6d0b9a73c63b096433056a16436734c9b
Pandonyx <pandonyx@gmail.com>
2023-03-01 05:36:35 -0800

    rev array

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
+void reverse_array(int *a, int n);
