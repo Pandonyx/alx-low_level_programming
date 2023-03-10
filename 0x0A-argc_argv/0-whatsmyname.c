894cd2deb11a43434978daa9977f197c1c4c806c
Pandonyx <pandonyx@gmail.com>
2023-03-10 08:39:37 -0800

    no name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of the program
+ *
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments passed to the program
+ *
+ * Return: Always 0
+ **/
+int main(int argc, char *argv[])
+{
+/* prints the name of the program */
+printf("%s\n", argv[0]);
+/* exit with status code 0 */
+return (0);
+}
