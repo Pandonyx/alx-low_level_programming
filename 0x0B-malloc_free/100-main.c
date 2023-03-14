8e4105f572379b9b2d1f9371de699fecadd23da5
Pandonyx <pandonyx@gmail.com>
2023-03-14 07:15:16 -0700

    how great are args

--- 0x0B-malloc_free/100-main.c
+++ 0x0B-malloc_free/100-main.c
@@ -19,8 +19,4 @@ int main(int ac, char *av[])
     printf("%s", s);
     free(s);
     return (0);
-}void free_grid(int **grid, int height);
-int **alloc_grid(int width, int height);
-char *str_concat(char *s1, char *s2);
-char *_strdup(char *str);
-char *create_array(unsigned int size, char c);
+}
