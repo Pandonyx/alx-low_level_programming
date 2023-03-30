0f2480ecfd05b0f98e49e1d4db580b1fcdcb852f
Pandonyx <pandonyx@gmail.com>
2023-03-30 15:46:44 -0700

    betty fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,17 @@
 section .data
-    hello db 'Hello, Holberton!', 0Ah, 0
-
+hello db 'Hello, Holberton!', 0Ah, 0
 section .text
-    global main
-    extern printf
-
+global main
+extern printf
 main:
-    push rbp
-    mov rbp, rsp
-    
-    ; Call printf function
-    mov rdi, hello
-    xor rax, rax
-    call printf
-    
-    ; Clean up and exit
-    mov rsp, rbp
-    pop rbp
-    xor eax, eax
-    ret
+push rbp
+mov rbp, rsp
+; Call printf function
+mov rdi, hello
+xor rax, rax
+call printf
+; Clean up and exit
+mov rsp, rbp
+pop rbp
+xor eax, eax
+ret
