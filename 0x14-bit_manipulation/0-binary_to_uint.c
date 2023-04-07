436b1b39b6ebdc4fd6fdd67b9c7a455288fa8294
Pandonyx <pandonyx@gmail.com>
2023-04-07 21:23:40 +0000

    betty fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,11 +16,11 @@ len++;
 do {
 len--;
 if (b[len] != '0' && b[len] != '1')
-return 0;
+return (0);
 if (b[len] == '1')
 total += power;
 power *= 2;
 } 
-while (len > 0);
+while (len > 0)
 return (total);
 }
