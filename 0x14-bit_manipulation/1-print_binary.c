09f7931d215d9630e376ce58bcf5c50223df667f
pandonyx <pandonyx@gmail.com>
2023-05-04 19:16:08 +0100

    c12 to c13 0x14-bit manipulation redone

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - print binary representation of a number
+ * @n: decimal number to print as binary
+ */
+void print_binary(unsigned long int n)
+{
+unsigned long int temp = n;
+int shifts = 0;
+if (n == 0)
+{
+printf("0");
+return;
+}
+do {
+shifts++;
+temp >>= 1;
+} while (temp > 0);
+do {
+shifts--;
+if ((n >> shifts) &1)
+printf("1");
+else
+printf("0");
+} while (shifts > 0);
+}
